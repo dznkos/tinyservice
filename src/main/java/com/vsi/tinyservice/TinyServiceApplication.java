11b41b47ddc3e0fe0914132b8e5d1351b16ae2fe
Luis vcn <khanccer@gmail.com>
2023-02-03 19:15:01 -0500

    tinyserv v1

---
+++ src/main/java/com/vsi/tinyservice/TinyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vsi.tinyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vsi/tinyservice/TinyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsi.tinyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
